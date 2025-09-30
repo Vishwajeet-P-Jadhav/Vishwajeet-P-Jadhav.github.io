66f85053e0b4f9231f0bf379f1db4b8a09f9d89b
Vishwajeet-P-Jadhav <122674956+Vishwajeet-P-Jadhav@users.noreply.github.com>
2025-09-30 21:25:40 +0000

    Deploying to gh-pages from @ Vishwajeet-P-Jadhav/Vishwajeet-P-Jadhav.github.io@883880c2a98f31f06f0e9cbb692098657409afc8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "ABOUT",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-experience",
-          title: "experience",
-          description: "A growing collection of your cool projects.",
+          title: "EXPERIENCE",
+          description: "Feel free to click and open individual entries.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
